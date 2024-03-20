c4745fe680ead22c69b4076198099ccfebf471d9
Carl Llemos <iamcarlllemos@gmail.com>
2024-03-20 20:36:50 +0800

    latest commit

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,13 +2,57 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\BranchModel;
+use App\Models\CourseModel;
+use App\Models\DepartmentModel;
+use App\Models\FacultyModel;
+use App\Models\StudentModel;
+use App\Models\SubjectModel;
 use Livewire\Component;
+
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Carbon;
 
 class Dashboard extends Component
 {
 
     public $data = [];
+
+
+    public function generate_colors() {
+        $response = Http::get('https://x-colors.yurace.pro/api/random?number=1');
+        return $response->json()['hex'];
+    }
+
+    public function fetch_data() {
+
+        $this->data['counts'] = [
+            'branch' => [
+                'count' => BranchModel::count(),
+                'color' => $this->generate_colors()
+            ],
+            'course' => [
+                'count' => CourseModel::count(),
+                'color' => $this->generate_colors()
+            ],
+            'department' => [
+                'count' => DepartmentModel::count(),
+                'color' => $this->generate_colors()
+            ],
+            'subject' => [
+                'count' => SubjectModel::count(),
+                'color' => $this->generate_colors()
+            ],
+            'faculty' => [
+                'count' => FacultyModel::count(),
+                'color' => $this->generate_colors()
+            ],
+            'student' => [
+                'count' => StudentModel::count(),
+                'color' => $this->generate_colors()
+            ],
+        ];
+    }
 
 
     public function render()
@@ -19,7 +63,7 @@ class Dashboard extends Component
 
         $mode_string = '';
 
-        if ($mode >= 5 && $mode < 12) {
+        if ($mode >= 1 && $mode < 12) {
             $mode_string = "good morning";
         } elseif ($mode >= 12 && $mode < 18) {
             $mode_string = "good afternoon";
@@ -35,6 +79,8 @@ class Dashboard extends Component
         $this->data = [
             'message' => $message,
         ];
+
+        $this->fetch_data();
 
         return view('livewire.admin.dashboard');
     }
--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'ASIA/MANILA'),
+    'timezone' => env('APP_TIMEZONE', ''),
 
     /*
     |--------------------------------------------------------------------------
