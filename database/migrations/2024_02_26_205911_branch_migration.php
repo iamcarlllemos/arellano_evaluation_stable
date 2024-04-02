31f4ecaed84c488e1e3023ed3e9dd7d714274c71
Carl Llemos <iamcarlllemos@gmail.com>
2024-04-02 18:14:54 +0800

    latest commit

--- database/migrations/2024_02_26_205911_branch_migration.php
+++ database/migrations/2024_02_26_205911_branch_migration.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('afears_branch', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image')->nullable;
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
