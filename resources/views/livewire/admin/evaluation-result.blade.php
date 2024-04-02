2867201cce0157df53d3eced74e6d9683eeac902
Carl Llemos <iamcarlllemos@gmail.com>
2024-04-02 20:27:10 +0800

    latest commit

--- resources/views/livewire/admin/evaluation-result.blade.php
+++ resources/views/livewire/admin/evaluation-result.blade.php
@@ -674,10 +674,8 @@
                                                 </a>
                                             </li>
                                         @empty
-                                            <li class="text-xs whitespace-nowrap">
-                                                <a wire:navigate href="javascript:void(0)" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-red-600">
-                                                    No records found
-                                                </a>
+                                            <li class="text-xs whitespace-nowrap text-red-700">
+                                                No records found
                                             </li>
                                         @endforelse
                                     </ul>
