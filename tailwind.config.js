fe33e42dab643ffe24b78781412921b3c30d4a10
Carl Llemos <iamcarlllemos@gmail.com>
2024-04-03 12:20:12 +0800

    latest commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-// import flowbite from 'flowbite/plugin';
+import flowbite from 'flowbite/plugin';
+import 'jstree';
+import 'flowbite/dist/flowbite.min.js';
 
 /** @type {import('tailwindcss').Config} */
 export default {
