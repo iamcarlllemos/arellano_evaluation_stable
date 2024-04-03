78d025a93cd4512016fe754a6d65dade2471ba3f
Carl Llemos <iamcarlllemos@gmail.com>
2024-04-03 12:08:22 +0800

    latest commit

--- resources/views/livewire/student/evaluate.blade.php
+++ resources/views/livewire/student/evaluate.blade.php
@@ -410,10 +410,10 @@
                     <p class="text-xs uppercase font-bold">Reference: #{{session('response')['faculty']['reference']}}</p>
                     <p class="text-xs uppercase font-bold text-slate-800">Submitted {{session('response')['faculty']['date_submitted']}}</p>
                 </div>
-                <div class="block sm:flex sm:justify-between mt-10 w-full">
+                <div class="sm:flex sm:justify-between mt-10">
                     <button
                         wire:click='move(3)'
-                        class="w-full sm:w-auto  items-center border border-sky-700 text-slate-900 bg-transparent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        class="w-full sm:w-auto items-center border border-sky-700 text-slate-900 bg-transparent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                         Previous
                     </button>
