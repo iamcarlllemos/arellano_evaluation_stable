99ea9844a9fec7281b68305d1e904a7dea4ed612
Carl Llemos <iamcarlllemos@gmail.com>
2024-04-03 11:54:43 +0800

    latest commit

--- resources/views/livewire/student/evaluate.blade.php
+++ resources/views/livewire/student/evaluate.blade.php
@@ -410,16 +410,16 @@
                     <p class="text-xs uppercase font-bold">Reference: #{{session('response')['faculty']['reference']}}</p>
                     <p class="text-xs uppercase font-bold text-slate-800">Submitted {{session('response')['faculty']['date_submitted']}}</p>
                 </div>
-                <div class="sm:flex justify-between mt-10">
+                <div class="sm:flex justify-between mt-10 w-full">
                     <button
                         wire:click='move(3)'
-                        class="w-full items-center border border-sky-700 text-slate-900 bg-transparent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        class="w-100 items-center border border-sky-700 text-slate-900 bg-transparent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                         Previous
                     </button>
                     <a wire:navigate
                         href="{{route('student.subject', ['evaluate' => $evaluate, 'semester' => $semester])}}"
-                        class="w-full sm:flex mt-3 sm:mt-0 text-white items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        class="w-100 mt-3 sm:mt-0 text-white  items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                         Return Subjects
                     </a>
